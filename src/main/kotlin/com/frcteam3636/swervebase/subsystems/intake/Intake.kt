c02d574dacb3a0071b19843af78d19f05505c72d
soanglengan <nongag.lesna@gmail.com>
2025-10-30 10:04:56 -0700

    started work on intake

--- src/main/kotlin/com/frcteam3636/swervebase/subsystems/intake/Intake.kt
+++ src/main/kotlin/com/frcteam3636/swervebase/subsystems/intake/Intake.kt
@@ -1,7 +1,22 @@
 package com.frcteam3636.swervebase.subsystems.intake
 
 import edu.wpi.first.wpilibj2.command.Subsystem
+import org.littletonrobotics.junction.Logger
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.Commands
 
 class Intake : Subsystem {
+    private val io = IntakeIOReal()
+
+    var inputs = LoggedIntakeInputs()
+
+    override fun periodic() {
+        io.updateInputs(inputs)
+        Logger.processInputs("intake", inputs)
+    }
+
+    fun intake() : Command = Commands.sequence(
+
+    )
 
 }
---
+++ src/main/kotlin/com/frcteam3636/swervebase/subsystems/intake/IntakeIO.kt
@@ -0,0 +1,34 @@
+package com.frcteam3636.swervebase.subsystems.intake
+
+import com.frcteam3636.swervebase.subsystems.indexer.IndexerInputs
+import edu.wpi.first.units.measure.Current
+import edu.wpi.first.units.measure.Voltage
+import org.team9432.annotation.Logged
+
+@Logged
+open class IntakeInputs {
+}
+
+interface IntakeIO {
+    fun setSpeed(percent: Double)
+    fun setCurrent(current: Current)
+    fun setVoltage(voltage: Voltage)
+    fun updateInputs(inputs: IntakeInputs)
+}
+
+class IntakeIOReal : IntakeIO {
+    override fun setSpeed(percent: Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCurrent(current: Current) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setVoltage(voltage: Voltage) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateInputs(inputs: IntakeInputs) {
+    }
+}
