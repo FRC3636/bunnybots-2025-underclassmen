645969eefa446dcfac4ff863f794a0375e8d3428
Tai Morioka <148292625+Taimorioka@users.noreply.github.com>
2025-11-12 17:44:04 -0800

    Update Shooter

--- src/main/kotlin/com/frcteam3636/swervebase/subsystems/shooter/Shooter.kt
+++ src/main/kotlin/com/frcteam3636/swervebase/subsystems/shooter/Shooter.kt
@@ -22,20 +22,13 @@ object Shooter : Subsystem {
     }
 
     fun intake() : Command = Commands.startEnd(
-        { io.setVoltage(2.0.volts) },
+        { io.setVoltage(4.0.volts, 3.6.volts) },
         { io.setVoltage(0.0.volts) }
     )
 
     fun outtake() : Command = Commands.startEnd(
-        { io.setVoltage((-2.0).volts) },
+        { io.setVoltage((-4.0).volts, (-3.6).volts) },
         { io.setVoltage(0.0.volts) }
     )
 
-    // Here is my sample code, should be adjusted further on to either
-    // be smart and wait for all to be shot, or just to use a good time
-    // value...
-    fun outtakeAllCarrots() : Command = Commands.race(
-        outtake(),
-        Commands.waitSeconds(1.0)
-    )
 }
