36e038f0222a8fda659dd2db1795f2a496fc010e
Tai Morioka <thetaimorioka@gmail.com>
2025-11-12 16:08:37 -0800

    small fix

--- src/main/kotlin/com/frcteam3636/swervebase/Robot.kt
+++ src/main/kotlin/com/frcteam3636/swervebase/Robot.kt
@@ -276,7 +276,6 @@ object Robot : LoggedRobot() {
     override fun autonomousInit() {
         if (beforeFirstEnable)
             beforeFirstEnable = true
-//        autoCommand = Dashboard.autoChooser.selected
         autoCommand?.schedule()
     }
 
