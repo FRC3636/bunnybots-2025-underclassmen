fb27f8a3b307470d93badd253b578a7494525373
Tai Morioka <thetaimorioka@gmail.com>
2025-12-13 08:32:07 -0800

    update canrange

--- src/main/kotlin/com/frcteam3636/swervebase/subsystems/indexer/IndexerIO.kt
+++ src/main/kotlin/com/frcteam3636/swervebase/subsystems/indexer/IndexerIO.kt
@@ -44,7 +44,8 @@ class IndexerIOReal : IndexerIO {
     private var canRange = CANrange(CTREDeviceId.CANRangeIndexer).apply {
         configurator.apply(
             CANrangeConfiguration().apply {
-                ProximityParams.ProximityThreshold = 0.1 // From the senior bot, may be different?
+                ProximityParams.ProximityThreshold = 0.12
+                ProximityParams.ProximityHysteresis = 0.01
                 ToFParams.UpdateMode = UpdateModeValue.ShortRange100Hz
             }
         )
